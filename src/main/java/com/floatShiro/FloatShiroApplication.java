2aea5953cbb7a0c0de8d295dbb16f48ca347ef01
bigskyfish <15939743797@163.com>
2018-11-06 00:31:12 +0800

    first shiro

---
+++ src/main/java/com/floatShiro/FloatShiroApplication.java
@@ -0,0 +1,15 @@
+package com.floatShiro;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+
+@SpringBootApplication
+public class FloatShiroApplication extends SpringBootServletInitializer {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(FloatShiroApplication.class, args);
+	}
+}
---
+++ src/test/java/com/floatShiro/FloatShiroApplicationTests.java
@@ -0,0 +1,16 @@
+package com.floatShiro;
+
+import org.junit.runner.RunWith;
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FloatShiroApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
